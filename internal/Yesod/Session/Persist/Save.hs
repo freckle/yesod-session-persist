3788d19f47cf6b80159e534714573641b040a709
Chris Martin <ch.martin@gmail.com>
2024-01-17 11:54:24 -0700

    drag storage save operation down into its own module

--- internal/Yesod/Session/Persist/Save.hs
+++ internal/Yesod/Session/Persist/Save.hs
@@ -7,21 +7,16 @@ import Internal.Prelude
 
 import Comparison
 import Control.Monad.State qualified as State
-import Control.Monad.Trans.Maybe (MaybeT (MaybeT), runMaybeT)
-import Data.Map.Strict qualified as Map
 import Session.Freeze
 import Session.KeyRotation
-import Time
 import Yesod.Core (SessionMap)
 import Yesod.Session.Embedding.Core
-import Yesod.Session.Key
 import Yesod.Session.Manager
 import Yesod.Session.Options
 import Yesod.Session.Persist.Load
 import Yesod.Session.SessionType
 import Yesod.Session.Storage.Operation
-import Yesod.Session.Timing.Options
-import Yesod.Session.Timing.Time
+import Yesod.Session.Storage.Save qualified as Storage
 
 data Save a
   = -- | Nothing was done because a session freeze was requested
@@ -51,101 +46,27 @@ saveSession
   -> SessionMap
   -> m (Save Session)
 saveSession SessionManager {options, storage, keyManager, runTransaction} load outputData =
-  let
-    ((requestedRotation, freeze), newInfo) =
-      flip State.runState outputData
-        $ (,)
-        <$> extractIgnoringError options.embedding.keyRotation
-        <*> extractIgnoringError options.embedding.freeze
-    autoRotation = options.keyRotationTrigger Comparison {old = loadedData load, new = newInfo}
-    rotation = requestedRotation <|> autoRotation
-  in
-    runTransaction
-      $ case freeze of
-        Just FreezeSessionForCurrentRequest -> pure Frozen
-        Nothing ->
-          let save oldSessionMaybe =
-                saveSessionOnDb
-                  options
-                  storage
-                  keyManager
-                  load.time
-                  newInfo
-                  oldSessionMaybe
-          in  case load.got of
-                Nothing -> save Nothing
-                Just s -> case rotation of
-                  Just RotateSessionKey -> do
-                    storage $ DeleteSession s.key
-                    saveResult <- save Nothing
-                    pure $ case saveResult of
-                      NoChange -> Deleted
-                      x -> x
-                  Nothing -> save (Just s)
+  runTransaction
+    $ case freeze of
+      Just FreezeSessionForCurrentRequest -> pure Frozen
+      Nothing ->
+        case (load.got, rotation) of
+          (Just s, Just RotateSessionKey) -> do
+            storage $ DeleteSession s.key
+            maybe Deleted Saved <$> save Nothing
+          _ -> maybe NoChange Saved <$> save load.got
+ where
+  ((requestedRotation, freeze), newInfo) =
+    flip State.runState outputData
+      $ (,)
+      <$> extractIgnoringError options.embedding.keyRotation
+      <*> extractIgnoringError options.embedding.freeze
 
--- | Save a session to the database
---
--- If an old session is supplied, it is replaced, otherwise a new session is generated.
--- If the session is empty, it is not saved and 'Nothing' is returned.
--- If the timeout resolution optimization is applied (cf. 'setTimeoutResolution'),
--- the old session is returned and no update is made.
-saveSessionOnDb
-  :: Monad tx
-  => Options tx m
-  -> (forall a. StorageOperation a -> tx a)
-  -> SessionKeyManager tx
-  -> UTCTime
-  -- ^ The current time
-  -> SessionMap
-  -- ^ The new session data to be saved
-  -> Maybe Session
-  -- ^ What's in the database
-  -> tx (Save Session)
-saveSessionOnDb options storage sessionKeyManager now newInfo oldSessionMaybe =
-  asumM
-    [ runMaybeT $ do
-        guardMaybeT $ isNothing oldSessionMaybe
-        guardMaybeT $ Map.null newInfo
-        pure NoChange
-    , runMaybeT $ do
-        -- If the data is the same and the old access time is within
-        -- the timeout resolution, just return the old session without
-        -- doing anything else.
-        res <- assertJust options.timing.resolution
-        old <- assertJust oldSessionMaybe
-        guardMaybeT $ old.map == newInfo
-        guardMaybeT $ diffUTCTime now old.time.accessed < res
-        pure NoChange
-    , runMaybeT $ do
-        oldSession <- assertJust oldSessionMaybe
-        let newSession =
-              Session
-                { key = oldSession.key
-                , map = newInfo
-                , time = Time {created = oldSession.time.created, accessed = now}
-                }
-        lift $ storage $ ReplaceSession newSession
-        pure $ Saved newSession
-    ]
-    `orElseM` do
-      sessionKey <- sessionKeyManager.new
-      let newSession =
-            Session
-              { key = sessionKey
-              , map = newInfo
-              , time = Time {created = now, accessed = now}
-              }
-      storage $ InsertSession newSession
-      pure $ Saved newSession
+  autoRotation =
+    options.keyRotationTrigger
+      Comparison {old = loadedData load, new = newInfo}
 
-orElseM :: Monad m => m (Maybe a) -> m a -> m a
-a `orElseM` b = a >>= maybe b pure
+  rotation = requestedRotation <|> autoRotation
 
-asumM :: Monad m => [m (Maybe a)] -> m (Maybe a)
-asumM = \case [] -> pure Nothing; x : xs -> x >>= maybe (asumM xs) (pure . Just)
-
-guardMaybeT :: Monad m => Bool -> MaybeT m ()
-guardMaybeT = \case True -> pure (); False -> MaybeT (pure Nothing)
-
-assertJust :: Monad m => Maybe a -> MaybeT m a
-assertJust = MaybeT . pure
+  save oldSessionMaybe =
+    Storage.save options storage keyManager load.time newInfo oldSessionMaybe
---
+++ internal/Yesod/Session/Storage/Save.hs
@@ -0,0 +1,82 @@
+module Yesod.Session.Storage.Save
+  ( save
+  ) where
+
+import Internal.Prelude
+
+import Control.Monad.Trans.Maybe (MaybeT (MaybeT), runMaybeT)
+import Data.Map.Strict qualified as Map
+import Time
+import Yesod.Core (SessionMap)
+import Yesod.Session.Key
+import Yesod.Session.Options
+import Yesod.Session.SessionType
+import Yesod.Session.Storage.Operation
+import Yesod.Session.Timing.Options
+import Yesod.Session.Timing.Time
+
+-- | Save a session to the database
+--
+-- Return value of 'Nothing' indicates that no changes were made.
+-- 'Just' is returned when a session was saved, either by an insert
+-- or a replace operation.
+save
+  :: Monad tx
+  => Options tx m
+  -> (forall a. StorageOperation a -> tx a)
+  -> SessionKeyManager tx
+  -> UTCTime
+  -- ^ The current time
+  -> SessionMap
+  -- ^ The new session data to be saved
+  -> Maybe Session
+  -- ^ What's in the database
+  -> tx (Maybe Session)
+save options storage sessionKeyManager now newInfo oldSessionMaybe =
+  asumM
+    [ runMaybeT $ do
+        guardMaybeT $ isNothing oldSessionMaybe
+        guardMaybeT $ Map.null newInfo
+        pure Nothing
+    , runMaybeT $ do
+        -- If the data is the same and the old access time is within
+        -- the timeout resolution, just return the old session without
+        -- doing anything else.
+        res <- assertJust options.timing.resolution
+        old <- assertJust oldSessionMaybe
+        guardMaybeT $ old.map == newInfo
+        guardMaybeT $ diffUTCTime now old.time.accessed < res
+        pure Nothing
+    , runMaybeT $ do
+        oldSession <- assertJust oldSessionMaybe
+        let newSession =
+              Session
+                { key = oldSession.key
+                , map = newInfo
+                , time = Time {created = oldSession.time.created, accessed = now}
+                }
+        lift $ storage $ ReplaceSession newSession
+        pure $ Just newSession
+    ]
+    `orElseM` do
+      sessionKey <- sessionKeyManager.new
+      let newSession =
+            Session
+              { key = sessionKey
+              , map = newInfo
+              , time = Time {created = now, accessed = now}
+              }
+      storage $ InsertSession newSession
+      pure $ Just newSession
+
+orElseM :: Monad m => m (Maybe a) -> m a -> m a
+a `orElseM` b = a >>= maybe b pure
+
+asumM :: Monad m => [m (Maybe a)] -> m (Maybe a)
+asumM = \case [] -> pure Nothing; x : xs -> x >>= maybe (asumM xs) (pure . Just)
+
+guardMaybeT :: Monad m => Bool -> MaybeT m ()
+guardMaybeT = \case True -> pure (); False -> MaybeT (pure Nothing)
+
+assertJust :: Monad m => Maybe a -> MaybeT m a
+assertJust = MaybeT . pure
